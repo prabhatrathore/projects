f844feaed414a68c27a0d69f17aeccb6adad59d5
Radhe Radhe <you@example.com>
2021-12-11 13:29:10 +0530

    intern

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -3,39 +3,47 @@ const shortid = require('shortid')
 
 const isValidRequestBody = function (requestBody) {
     return Object.keys(requestBody).length > 0
-}
-const isValidUrl = function isValidURL(string) {
-    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    return (res !== null)
 };
+
+const isUrl= function isURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return pattern.test(str);
+  };
+
 const baseUrl = 'http:localhost:3000'
 const createUrl = async function (req, res) {
     try {
         let body = req.body;
+        // console.log(body)
         if (!isValidRequestBody(body)) {
             res.status(400).send({ status: false, msg: "provide  details" })
-            return };
+            return
+        };
 
-        const Url = req.body.longUrl;
-        const urlCode = shortid.generate();
-
-        if (isValidUrl(Url)) {
-            let urlExist = await urlModel.findOne({ longUrl: Url })
+        const longUrl = req.body.longUrl;
+        const urlCode = shortid.generate().toLowerCase().replace(/[0-9]/g, '').replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
+        if (isUrl(longUrl)) {
+            let urlExist = await urlModel.findOne({ longUrl:longUrl })
             if (urlExist) {
                 res.status(200).send({ status: true, data: urlExist })
             } else {
                 const shortUrl = baseUrl + '/' + urlCode
-
-                let urlOf = new urlModel({ longUrl, shortUrl, urlCode, date: new Date() })
+                let urlOf = new urlModel({ longUrl,shortUrl, urlCode, date: new Date() })
                 let data = await urlModel.create(urlOf)
                 res.status(200).send({ status: true, data: data })
             }
-        }else{
-            res.status(400).send({status:false,msg:"not valid url"})
+        } else {
+            res.status(400).send({ status: false, msg: "not valid url" })
         }
     }
     catch (err) {
-        res.status(500).send({ status: false, msg: err.message })
+        console.log(err)
+        res.status(500).send({ status: false, msg: err.message });
     }
 }
 const getUrl = async function (req, res) {
