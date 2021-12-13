016cc465ee2c161ea51372cae6b8407fe486ceb5
Radhe Radhe <you@example.com>
2021-12-13 10:58:47 +0530

    phase1

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,39 +1,54 @@
 const urlModel = require('../models/urlModel')
 const shortid = require('shortid')
+const validUrlData = require('valid-url')
 
 const isValidRequestBody = function (requestBody) {
     return Object.keys(requestBody).length > 0
 };
-
-const isUrl= function isURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return pattern.test(str);
-  };
-
 const baseUrl = 'http:localhost:3000'
 const createUrl = async function (req, res) {
     try {
-        let body = req.body;
+        let requestBody = req.body;      //1
         // console.log(body)
-        if (!isValidRequestBody(body)) {
+        if (!isValidRequestBody(requestBody)) {
             res.status(400).send({ status: false, msg: "provide  details" })
             return
         };
 
-        const longUrl = req.body.longUrl;
-        const urlCode = shortid.generate().toLowerCase().replace(/[0-9]/g, '').replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
-        if (isUrl(longUrl)) {
-            let urlExist = await urlModel.findOne({ longUrl:longUrl })
+        const longUrl = req.body.longUrl;    //2
+
+        function ValidURL(longUrl) {
+            var regex = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+            if (!regex.test(longUrl)) {
+                return false;
+            } else {
+                return true;
+            }
+        };
+        if (longUrl.includes("w") && (longUrl.indexOf("w") === 6 || longUrl.indexOf("w") === 7 || longUrl.indexOf("w") === 8)) {
+            let arr = [];
+            let i = longUrl.indexOf("w");
+            while (longUrl[i] == "w") {
+                if (longUrl[i] === "w") { arr.push(longUrl[i]) };
+                i++
+            }
+            if (!(arr.length === 3)) { return res.status(400).send({ status: false, msg: "Url is not valid " }) }
+        }
+        if (!(/(.com|.org|.co.in|.in|.co|.us)/.test(longUrl))) {
+            return res.status(400).send({msg:"Url is not valid2"})
+        }
+
+        if (ValidURL(longUrl)) {
+            let urlExist = await urlModel.findOne({ longUrl: longUrl });
+
             if (urlExist) {
+
                 res.status(200).send({ status: true, data: urlExist })
             } else {
+                const urlCode = shortid.generate().toLowerCase().replace(/[0-9]/g, '').replace(/[&\/\\#,+()$~%.-_-'":*?<>{}]/g, '');   //3
+
                 const shortUrl = baseUrl + '/' + urlCode
-                let urlOf = new urlModel({ longUrl,shortUrl, urlCode, date: new Date() })
+                let urlOf = new urlModel({ longUrl, shortUrl, urlCode })
                 let data = await urlModel.create(urlOf)
                 res.status(200).send({ status: true, data: data })
             }
@@ -49,12 +64,12 @@ const createUrl = async function (req, res) {
 const getUrl = async function (req, res) {
     try {
         const url = await urlModel.findOne({ urlCode: req.params.code })
-
+        console.log(url)
         if (url) {
-            return res.redirect(url.longUrl)
+            return res.redirect( url.longUrl);
 
         } else {
-            return res.status(404).json('No URL Found')
+            return res.status(404).send({ msg: 'No URL Found' })
         }
     } catch (err) {
         res.status(500).send({ status: false, msg: err.message })
@@ -62,3 +77,8 @@ const getUrl = async function (req, res) {
 }
 module.exports.getUrl = getUrl
 module.exports.createUrl = createUrl
+
+
+
+
+
--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
 const urlSchema = new mongoose.Schema({
-    urlCode: {type: String,required:true, lowercase:true,trim :true},
-    longUrl:    {type: String,required:true,trim :true},
-    shortUrl: {type: String,required:true, uniques:true},
-    clickCount: Number
-} , { timestamps: true } );
+    urlCode: { type: String, required: true, lowercase: true, trim: true },
+    longUrl: { type: String, required: true, trim: true },
+    shortUrl: { type: String, required: true, uniques: true },
+  
+}, { timestamps: true }
+);
+function ValidURL(str) {
+    var regex = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    if(!regex .test(str)) {
+      alert("Please enter valid URL.");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
 module.exports = mongoose.model("url", urlSchema);
