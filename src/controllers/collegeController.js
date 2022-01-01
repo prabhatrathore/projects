cd76189c024811bd8e155644a3d8531ca787ca71
prabhatrathore <93571813+prabhatrathore@users.noreply.github.com>
2022-01-01 15:51:51 +0530

    Update collegeController.js

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const collegeModel = require('../models/collegeModel')
-const ObjectId = mongoose.Types.ObjectId
+
 
 const createColleges = async function (req, res) {
     const isValid = function (value) {
@@ -11,45 +11,47 @@ const createColleges = async function (req, res) {
     const isValidRequestBody = function (requestBody) {
         return Object.keys(requestBody).length > 0
     }
-    const isAlpha = str => /^[a-zA-Z]*$/.test(str);
+    const isAlpha = /^[a-zA-Z]*$/;
+    
     try {
         const requestBody = req.body;
         if (!isValidRequestBody(requestBody)) {
             res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide college details' })
             return
         }
+        
         let temp = requestBody.name
         if (!isValid(temp)) {
             res.status(400).send({ status: false, message: 'Name is required' })
             return
-        } // in case there is a spacing in collge abbreviation 
-        let properspacing = temp.replace(/\s+/g, '') // this will remove all spacing by recursively/ constinously calling regex untill there is no space in b/w
-        if (!isAlpha(properspacing)) {
+        };
+        let properspacing = temp.replace(/\s+/g, '')
+        if (!(isAlpha.test(properspacing))) {
             res.status(400).send({ err: "Invalid request parameters-> Please provide valid college abbreviation. The abbreviation name should only contains alphabets. " })
             return
-        } //in case the name is not in lower case 
+        };
         let Name = properspacing.toUpperCase()
         requestBody.name = Name
-        // Extract params
+ 
         const { name, fullName, logo } = requestBody;
         if (!isValid(logo)) {
             res.status(400).send({ status: false, message: 'logo is required' })
             return
-        }
+        };
         if (!isValid(fullName)) {
             res.status(400).send({ status: false, message: 'fullName is required' })
             return
-        }  // cleaning up the full name     
-        let tempN = req.body.fullName
+        };     
+        let tempN = req.body.fullName;
         let tempname = tempN.replace(/_/g, ' ') // replacing all underscore with space
         const collegeData = {
             name: name, fullName: tempname, logo: logo
-        }
+        };
         let data = await collegeModel.create(collegeData);
         res.status(201).send({ status: true, data: data })
-    }
+    };
     catch (err) {
         res.status(500).send({ msg: err.message })
-    }
-}
+    };
+};
 module.exports.createColleges = createColleges;
